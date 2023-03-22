c2a710a3e58cb7dc48faf518150e45aaf9967859
Dmitriy Andrianov <dandrianov@srv.rbt>
2023-03-22 14:48:52 +0300

    ДЗ к семинару 08

--- pyton-seminar/seminar_python_8.py
+++ pyton-seminar/seminar_python_8.py
@@ -1,20 +1,19 @@
-def read_last(lines, file):
-    if lines > 0:
-        with open('article.txt', encoding='utf-8') as text:
-            file_lines = text.readlines()[-lines:]
-        for line in file_lines:
-            print(line.strip())
-        else:
-            print('Количество строк может быть только целым положительным')
- 
- 
-# Тесты
-read_last(3, 'article.txt')
-read_last(-5, 'article.txt')
+# def read_last(lines, file):
+#     if lines > 0:
+#         with open('article.txt', encoding='utf-8') as text:
+#             file_lines = text.readlines()[-lines:]
+#         for line in file_lines:
+#             print(line.strip())
+#         else:
+#             print('Количество строк может быть только целым положительным')
 
 
+# # Тесты
+# read_last(3, 'article.txt')
+# read_last(-5, 'article.txt')
 
-#article.txt
+
+# article.txt
 # Вечерело
 # Жужжали мухи
 # Светил фонарик
@@ -23,3 +22,16 @@ read_last(-5, 'article.txt')
 # Деревья шумели
 # Тучи разошлись
 # Листва зеленела
+
+
+# Задача 2
+with open('article.txt') as file:
+    for line in file.readlines():
+        if len(line) > max_len:
+            max_len = len(line)
+            max_str = [line]
+        elif len(line) == max_len:
+            max_str.append(line)
+
+        for elem in max_str:
+            print(elem, end='article.txt')
